13bfc937e3138a0371a094a8f403d33a9457bba0
Hitesh <hiteshsaibv.24cs@saividya.ac.in>
2026-05-08 13:09:23 +0530

    frontend code

---
+++ dcrm/website/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('add_record/', views.add_record, name='add_record'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    
+]
